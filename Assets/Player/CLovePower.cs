cf56bccc714df967e6350850cd5e4013318bb293
hell25mk <hell25mk@gmail.com>
2018-08-28 16:49:10 +0900

    [add] ファイルの追加
    LovePowerとLiveFireを追加

---
+++ Assets/Player/CLovePower.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CLovePower : MonoBehaviour {
+
+    [SerializeField] private Text lpPoint;
+    public float lovePower {
+        get {
+            return lovePower;
+        }
+        set {
+            lovePower = value;
+        }
+    }
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+
+        this.lpPoint.text = GetComponent<Text>().text;
+        this.lovePower = 100.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.lpPoint.text = "LP" + this.lovePower.ToString("f1") + "%";
+
+        timer -= Time.deltaTime;
+        if(timer <= 0.0f) {
+            timer = 1.0f;
+            this.lovePower -= 0.1f;
+        }
+
+	}
+
+}
