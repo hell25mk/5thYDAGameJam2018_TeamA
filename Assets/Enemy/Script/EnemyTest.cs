1f039bf955c6280b4ebf7bd0ad43617b12d8c242
DA\nwuser <torakichi19981017@gmail.com>
2018-08-28 17:06:43 +0900

    [add]　script
    スクリプトの追加

---
+++ Assets/Enemy/Script/EnemyTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTest : MonoBehaviour {
+
+	private float x;
+	private float y;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		x = this.transform.position.x;
+		y = this.transform.position.y;
+
+		this.transform.position = new Vector2(x - 0.01f, y);
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Destroy (gameObject);
+		}
+	}
+}
