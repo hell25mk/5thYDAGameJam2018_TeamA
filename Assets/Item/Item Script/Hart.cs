ea9e0e38e68d2ca03d629b1b6837be4d8faf086e
AKIFUMI6424 <akiaki.freedom.6424@gmail.com>
2018-08-28 17:42:20 +0900

    [add] イメージの追加
    ハートの画像と当たり判定のソース

---
+++ Assets/Item/Item Script/Hart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hart : MonoBehaviour {
+
+    //取得された
+    bool isGotten;
+
+    [SerializeField]
+	int score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    /*
+        public bool IsGotten(){
+            return isGotten;
+        }
+
+        public int GetScore(){
+            return score;
+        }
+    */
+}
