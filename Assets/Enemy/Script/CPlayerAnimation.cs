acc12ada8c2d5595bde435d257bb198f8690a6c6
DA\nwuser <torakichi19981017@gmail.com>
2018-08-28 19:53:27 +0900

    [add] script
    アニメーションのscript

---
+++ Assets/Enemy/Script/CPlayerAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayerAnimation : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			animator.SetBool ("Rigth_walk", true);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			animator.SetBool ("Left_walk", true);
+		}
+	}
+}
