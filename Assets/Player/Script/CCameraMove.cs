4ce45017b0139684ac36de93b466bef6b278ab0f
hell25mk <hell25mk@gmail.com>
2018-08-29 13:15:21 +0900

    [add] Cameraのスクリプトの追加
    自動スクロール用カメラの追加

---
+++ Assets/Player/Script/CCameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCameraMove : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position = new Vector3(this.transform.position.x + 0.05f, 0.0f, -10.0f);
+
+	}
+}
