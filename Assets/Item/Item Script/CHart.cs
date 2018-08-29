f97f8b07d4bac910dafcbf928c8bb6146749d69f
hell25mk <hell25mk@gmail.com>
2018-08-29 14:50:35 +0900

    [update] 更新
    更新

---
+++ Assets/Item/Item Script/CHart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHart : MonoBehaviour {
+
+    GameObject player1;
+    GameObject player2;
+
+    CLovePower p1;
+    CLovePower p2;
+
+    // Use this for initialization
+    void Start() {
+
+        player1 = GameObject.Find("PlayerWoman");
+        player2 = GameObject.Find("PlayerMan");
+
+        p1 = player1.GetComponent<CLovePower>();
+        p2 = player2.GetComponent<CLovePower>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if(collision.gameObject.tag == "PlayerWoman") {
+            p1.LovePowerUp(20.0f);
+        }
+
+        if(collision.gameObject.tag == "PlayerMan") {
+            p2.LovePowerUp(20.0f);
+        }
+
+    }
+
+}
---
+++ Assets/UI/Script/CGameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CGameOver : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if(collision.gameObject.tag == "PlayerWoman") {
+            SceneManager.LoadScene("GameOver");
+        }
+
+        if(collision.gameObject.tag == "PlayerMan") {
+            SceneManager.LoadScene("GameOver");
+        }
+
+    }
+
+}
