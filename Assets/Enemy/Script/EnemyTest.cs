d1ebc1093d8c4696be77c6ef86023edbd0a49b33
DA\nwuser <torakichi19981017@gmail.com>
2018-08-29 10:15:47 +0900

    [update] script
    左右移動の追加

--- Assets/Enemy/Script/EnemyTest.cs
+++ Assets/Enemy/Script/EnemyTest.cs
@@ -6,9 +6,16 @@ public class EnemyTest : MonoBehaviour {
 
 	private float x;
 	private float y;
+	private int []EnemyMovePatern;
+	private int MovePaternNum;
 
 	// Use this for initialization
 	void Start () {
+		
+		EnemyMovePatern = new int[] { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2 };
+		MovePaternNum = 0;
+
+
 
 	}
 
@@ -18,7 +25,24 @@ public class EnemyTest : MonoBehaviour {
 		x = this.transform.position.x;
 		y = this.transform.position.y;
 
-		this.transform.position = new Vector2(x - 0.01f, y);
+		//this.transform.position = new Vector2(x - 0.01f, y);
+
+		switch (EnemyMovePatern[MovePaternNum]) {
+		case 1: 
+			this.transform.position = new Vector2 (x - 0.01f, y);
+			MovePaternNum++;
+			break;
+
+		case 2:
+			this.transform.position = new Vector2 (x + 0.01f, y);
+			MovePaternNum++;
+			break;
+		}
+		if (MovePaternNum > EnemyMovePatern.Length-2) {
+			MovePaternNum = 0;
+		}
+
+		Debug.Log (MovePaternNum);
 
 	}
 
